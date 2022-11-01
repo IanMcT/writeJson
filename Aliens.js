0eaba4373749c96f6c8d0c685c2b79a19b1ec4b6
IanMcT <ian.mctavish@tldsb.on.ca>
2022-11-01 14:31:11 +0000

    Initial commit

---
+++ Aliens.js
@@ -0,0 +1,9 @@
+class Alien{
+  constructor(x,y, alienName){
+    this.x = x;
+    this.y = y;
+    this.health = 100;
+    this.alienName = alienName;
+  }
+  
+}
---
+++ script.js
@@ -0,0 +1,4 @@
+const aliens = [new Alien(0,0,"Bob"), new Alien(10,0,"Suzy")];
+alert(aliens);
+const myJSON = JSON.stringify(aliens);
+alert(myJSON);
